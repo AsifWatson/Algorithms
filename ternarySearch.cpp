3a8f5d3c731e8f7604e8c5625a2f8ef8a117260e
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2019-03-30 16:00:55 +0600

    Update ternarySearch.cpp

--- ternarySearch.cpp
+++ ternarySearch.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+struct point
+{
+    double x,y,z;
+};
+
+double dist(point p1,point p2)
+{
+    return sqrt(((p1.x-p2.x)*(p1.x-p2.x))+((p1.y-p2.y)*(p1.y-p2.y))+((p1.z-p2.z)*(p1.z-p2.z)));
+}
+
+point p[4];
+
 double ternarySearch()
 {
     double left,right;
@@ -8,28 +21,25 @@ double ternarySearch()
 
     while(num--)
     {
-        point fa,la,fc,lc;
+        point fa,la;
         fa.x=(2*p[1].x + p[2].x)/3.0;
         fa.y=(2*p[1].y + p[2].y)/3.0;
+        fa.z=(2*p[1].z + p[2].z)/3.0;
         la.x=(p[1].x + 2*p[2].x)/3.0;
         la.y=(p[1].y + 2*p[2].y)/3.0;
+        la.z=(p[1].z + 2*p[2].z)/3.0;
 
-        fc.x=(2*p[3].x + p[4].x)/3.0;
-        fc.y=(2*p[3].y + p[4].y)/3.0;
-        lc.x=(p[3].x + 2*p[4].x)/3.0;
-        lc.y=(p[3].y + 2*p[4].y)/3.0;
+        left=dist(fa,p[3]);
+        right=dist(la,p[3]);
 
-        left=dist(fa,fc);
-        right=dist(la,lc);
-
-        if(left>right){p[1]=fa; p[3]=fc;}
-        else{p[2]=la; p[4]=lc;}
+        if(left>right){p[1]=fa;}
+        else{p[2]=la;}
     }
+
     return abs(left+right)/2;
 }
 
 int main()
 {
-
-    return 0;
+    ternarySearch();
 }
