e72d69080a384a9e846dba80bfaafeef2b17d195
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2019-03-30 16:02:04 +0600

    Update ternarySearch.cpp

--- ternarySearch.cpp
+++ ternarySearch.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 struct point
 {
-    double x,y,z;
+    double x,y;
 };
 
 double dist(point p1,point p2)
 {
-    return sqrt(((p1.x-p2.x)*(p1.x-p2.x))+((p1.y-p2.y)*(p1.y-p2.y))+((p1.z-p2.z)*(p1.z-p2.z)));
+    return sqrt(((p1.x-p2.x)*(p1.x-p2.x))+((p1.y-p2.y)*(p1.y-p2.y)));
 }
 
 point p[4];
@@ -24,11 +24,10 @@ double ternarySearch()
         point fa,la;
         fa.x=(2*p[1].x + p[2].x)/3.0;
         fa.y=(2*p[1].y + p[2].y)/3.0;
-        fa.z=(2*p[1].z + p[2].z)/3.0;
+        
         la.x=(p[1].x + 2*p[2].x)/3.0;
         la.y=(p[1].y + 2*p[2].y)/3.0;
-        la.z=(p[1].z + 2*p[2].z)/3.0;
-
+        
         left=dist(fa,p[3]);
         right=dist(la,p[3]);
 
