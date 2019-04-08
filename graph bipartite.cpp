47fc0f95c58fc909a6962ef75f6e2a371caf0db5
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2019-04-09 03:08:39 +0600

    Update graph bipartite.cpp

--- graph bipartite.cpp
+++ graph bipartite.cpp
@@ -12,7 +12,7 @@ void addEdge(int v,int w)
     graph[w].push_back(v);
 }
 
-void bfs(int src)
+bool bipartite(int src)
 {
 	memset(color,0,sizeof(color));
 	memset(visited,0,sizeof(visited));
@@ -25,43 +25,25 @@ void bfs(int src)
 
 	while(!q.empty())
 	{
-	    int u=q.front();
-            q.pop();
-	    for(int i=0;i<graph[u].size();i++)
-            {
-	        int v=graph[u][i];
-	        if(!visited[v])
-	        {
-		    if(color[u]==1)color[v]=0;
-		    if(color[u]==0)color[v]=1;
-		    visited[v]=1;
-		    q.push(v);
-	        }
-            }
-	}
-}
-
-bool bipartite(int src)
-{
-    memset(visited,0,sizeof(visited));
-
-    visited[src]=1;
-
-    queue<int> q;
-    q.push(src);
-
-    while(!q.empty())
-    {
-        int u=q.front();
-        q.pop();
-        for(int i=0;i<graph[u].size();i++)
-        {
+		int u=q.front();
+		q.pop();
+		for(int i=0;i<graph[u].size();i++)
+		{
             int v=graph[u][i];
-            if(color[v]==color[u])return false;
-            if(visited[v]==0){visited[v]=1;q.push(v);}
-        }
-    }
-    return true;
+            if(color[u]==1)
+            {
+                if(color[v]==1)return false;
+                else color[v]=2;
+            }
+            if(color[u]==2)
+            {
+                if(color[v]==2)return false;
+                else color[v]=1;
+            }
+            if(!visited[v]){visited[v]=1;q.push(v);}
+		}
+	}
+	return true;
 }
 
 int main()
@@ -74,8 +56,6 @@ int main()
         cin>>a>>b;
         addEdge(a,b);
     }
-
-	bfs(1);
 
     if(bipartite(1))cout<<"Bipartite"<<endl;
     else cout<<"Not Bipartite"<<endl;
