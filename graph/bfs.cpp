412a753e3c2f956bf855d549e83675999818e7ea
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2019-06-15 00:39:09 +0600

    Update bfs.cpp

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -1,63 +1,51 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<int> adj[100]; //adj[a].push_back(b); for add an edge from a to b
-int visited[100]={0}; //O if not visited, 1 if visited
-int level[100];
+vector<int> graph[100000];
+int visited[100000]={0};
+int level[100000];
 
 void addEdge(int v, int w)
 {
-    adj[v].push_back(w); // Add w to v’s list.
+    graph[v].push_back(w);
+    graph[w].push_back(v);
 }
 
-void bfs(int s, int n)
+void bfs(int s,int n)
 {
-    for(int i=0; i<n; i++)
-        visited[i] = 0;
+    for(int i=0;i<n;i++)visited[i]=0;
 
-    queue<int>Q;
-    Q.push(s);
-    visited[s] = 1;
-    level[s] = 0;
+    queue<int>q;
+    q.push(s);
+    visited[s]=1;
+    level[s]=0;
 
-    while(!Q.empty())
+    while(!q.empty())
     {
-        int u = Q.front();
-        cout << u <<" ";
+        int u=q.front();
 
-        for(int i=0; i<adj[u].size(); i++)
+        for(int i=0;i<graph[u].size();i++)
         {
-            if(visited[adj[u][i]]==0)
+            if(!visited[graph[u][i]])
             {
-                int v = adj[u][i];
-                level[v] = level[u]+1;
-                visited[v] = 1;
-                Q.push(v);
+                int v=graph[u][i];
+                level[v]=level[u]+1;
+                visited[v]=1;
+                q.push(v);
             }
         }
-        Q.pop();
-    }
-
-    cout<<endl;
-
-    for(int i=0; i<n; i++)
-    {
-        printf("%d to %d distance = %d\n", s, i, level[i]);
+        q.pop();
     }
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
-
-    addEdge(0, 1);
-    addEdge(0, 2);
-    addEdge(1, 2);
-    addEdge(2, 0);
-    addEdge(2, 3);
-    addEdge(3, 4);
+    addEdge(0,1);
+    addEdge(0,2);
+    addEdge(1,2);
+    addEdge(2,0);
+    addEdge(2,3);
+    addEdge(3,4);
 
     bfs(2, 4);
     return 0;
