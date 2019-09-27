bf3938d97f3312e112f86707e1110049f239a3a5
Md Ashfaq Islam Asif <ashfaqislam33@gmail.com>
2019-09-27 17:47:04 +0600

    Update kruskal.cpp

--- graph/kruskal.cpp
+++ graph/kruskal.cpp
@@ -1,79 +1,68 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
-struct edge
-{
-    int u,v,w;
-};
-
-vector<edge>graph,output;
-int parent[200005],mstValue=0;
-
-bool compare(edge a, edge b)
-{
-    return a.w<b.w;
-}
+int parent[105];
 
 int Find(int r)
 {
-    if(parent[r]==r)
-        return r;
+    if(parent[r]==r)return r;
     return parent[r]=Find(parent[r]);
 }
 
-void initPar(int r)
+void initPar(int n)
 {
-    for(int i=0 ; i<r ; i++)
-    {
-        parent[i]=i;
-    }
+    for(int i=0 ; i<n ; i++)parent[i]=i;
 }
-void kruskal(int n)
+
+struct data
 {
-    sort(graph.begin(), graph.end(), compare);
+    int u,v;
+    double w;
+    bool operator < (const data &p) const
+    {
+        return w < p.w;
+    }
+};
+
+double kruskal(int n, vector<data> graph, int mstValue=0)
+{
+    sort(graph.begin(),graph.end());
     initPar(n);
-    int cnt=0;
-    for(int i=0 ; i<(int)graph.size() ; i++)
+
+    for(int i=0; i<graph.size(); i++)
     {
         int uPr=Find(graph[i].u);
         int vPr=Find(graph[i].v);
+
         if(uPr!=vPr)
         {
-            if(cnt==n-1)
-                break;
-            output.push_back(graph[i]);
-            mstValue+=graph[i].w;
             parent[uPr]=vPr;
-            cnt++;
+            mstValue+=graph[i].w;
+            //output.push_back(graph[i]);  // if needed to show the tree.
         }
     }
-
+    return mstValue;
 }
-
 
 int main()
 {
-    int n,e;
-    cin>>n>>e;
+    int n;
+    cin>>n;
 
-    for(int i=0 ; i<e ; i++)
+    vector<data> graph;
+
+    for(int i=0; i<n; i++)
     {
         int u,v,w;
         cin>>u>>v>>w;
-        edge input;
+        data input;
         input.u=u;
         input.v=v;
         input.w=w;
         graph.push_back(input);
     }
 
-    kruskal(n);
-
-    cout<<"MST Value : "<<mstValue<<endl;
-    for(int i=0 ; i<(int)output.size() ; i++)
-    {
-        cout<<output[i].u<<" "<<output[i].v<<" "<<output[i].w<<endl;
-    }
+    cout<<fixed<<setprecision(2)<<kruskal(n,graph)<<endl;
 
     return 0;
 }
